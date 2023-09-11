2ce8b4aa9bf4b5b9690963443882d72d34b18f00
Ny19RáczD <Ny19RczD@203-4>
2023-09-11 09:00:03 +0200

    Add project files.

---
+++ ConsoleApp52/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp52
+{
+    class Class1
+    {
+    }
+}
---
+++ ConsoleApp52/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp65;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ConsoleApp65.Kategoria> kategoriak = new List<ConsoleApp65.Kategoria>();
+            StreamReader streamReader = new StreamReader(
+                            path: @"..\..\src\titanic.txt",
+                            encoding: System.Text.Encoding.UTF8);
+            var src = streamReader;
+            while (!src.EndOfStream)
+            {
+                string sor = src.ReadLine();
+                var kat = new Kategoria(sor);
+                kategoriak.Add(kat);
+            }
+            Console.WriteLine($"{kategoriak.Count}");
+            Console.ReadKey(true);
+
+        }
+    }
+}
+
